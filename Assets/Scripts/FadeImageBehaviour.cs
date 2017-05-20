56ca6e8fcc919aae158be322479c510f93239928
G2 <g2simm+bow@gmail.com>
2017-05-20 20:04:36 +0900

    Add fadein script

---
+++ Assets/Scripts/FadeImageBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeImageBehaviour : MonoBehaviour
+{
+
+    [SerializeField]
+    private Color fadeColor;
+    [SerializeField]
+    private float fadeSpeed = 0.03f;
+    
+    private void Start()
+    {
+        fadeColor = this.GetComponent<Image>().color;
+        StartCoroutine(StartFade());
+    }
+
+    private IEnumerator StartFade()
+    {
+        while (true)
+        {
+            fadeColor.a -= fadeSpeed;
+            if (fadeColor.a < 0)
+            {
+                yield break;
+            }
+
+             yield return this.GetComponent<Image>().color = fadeColor;
+        }
+    }
+}
